f3190086839d60900814745d996faf2ff3c82496
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2022-08-02 10:31:15 +0200

    Properly cache the images by default

--- src/miniwdl_slurm/slurm_singularity.py
+++ src/miniwdl_slurm/slurm_singularity.py
@@ -19,18 +19,18 @@
 # SOFTWARE.
 
 import logging
+import os.path
 import sys
 from typing import Callable, Dict
 
-import WDL.runtime.backend.singularity
+from WDL.runtime.backend.singularity import SingularityContainer
 from WDL.runtime import config
 from WDL.runtime.backend.cli_subprocess import _SubprocessScheduler
 
 
-class SlurmSingularityRun(WDL.runtime.backend.singularity.SingularityContainer):
+class SlurmSingularityRun(SingularityContainer):
     @classmethod
     def global_init(cls, cfg: config.Loader, logger: logging.Logger) -> None:
-        super().global_init(cfg, logger)
 
         # TODO: Query from cluster. This requires parsing sinfo output and
         # determining which partition etc. etc.
@@ -40,9 +40,19 @@ class SlurmSingularityRun(WDL.runtime.backend.singularity.SingularityContainer):
             "time": sys.maxsize,
         }
         _SubprocessScheduler.global_init(cls._resource_limits)
+        # Since we run on the cluster, the images need to be placed in a
+        # shared directory. The singularity cache itself cannot be shared
+        # across nodes, as it can become corrupted when nodes pull the same
+        # image. The solution is to pull image to a shared directory on the
+        # submit node. If no image_cache is given, simply place a folder in
+        # the current working directory.
+        if cfg.get("singularity", "image_cache") == "":
+            cfg.override({"singularity":
+                              {"image_cache": "miniwdl_singularity_cache"}})
+        SingularityContainer.global_init(cfg, logger)
 
     @classmethod
     def detect_resource_limits(cls, cfg: config.Loader,
                                logger: logging.Logger) -> Dict[str, int]:
-        return super().detect_resource_limits(cfg, logger)
+        return cls._resource_limits
 
